ce246aa1be258927195fbcaa975f78b2c7ab13b8
Mnyoki <mosesmunyoki6@gmail.com>
2022-01-13 02:47:21 -0500

    tic tac toe game

---
+++ src/components/Box.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Box({ value, onClick }) {
+  const style = {
+    height: "70px",
+    width: "70px",
+    padding: 0,
+    margin: "8px",
+    borderRadius: "5px",
+    backgroundColor: "rgb(51, 209, 118)",
+    color: "#765336",
+    fontWeight: "900",
+    border: 0,
+    cursor: "pointer",
+    fontSize: "1rem",
+  };
+
+  return (
+    <button style={style} onClick={onClick}>
+      {value}
+    </button>
+  );
+}
+
+export default Box;
